ca738db38c856d339630550d90942bfd8d45749f
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-25 20:29:46 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "gunan", "pwd" => "lee2020!", "Database" => "food", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:gunanblob.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    $row = 1;
+    $handle = fopen("myfile.csv", "r+");
+    //$sql = "INSERT INTO latlng VALUES ('".$data[0]."','".$data[1]."');";
+    while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+        $num = count($data);
+
+        $row++;
+
+        $sql = "INSERT INTO food VALUES (";
+
+        for ($c=0; $c < $num; $c++) {
+            $sql .= "'" . $data[$c] . "'";
+            if($c+1 !== $num){
+                $sql .= ", ";
+            }
+        }
+        $sql .= ");";
+        $getResults = sqlsrv_query($conn,$sql);
+        //echo "$sql<br />";
+    }
+    fclose($handle);
+?>

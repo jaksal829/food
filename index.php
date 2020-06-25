f9394c5199c201571eb97a859ebad21147b04b93
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-25 21:03:27 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?
 // SQL Server Extension Sample Code:
+
 $connectionInfo = array("UID" => "gunan", "pwd" => "lee2020!", "Database" => "food", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:gunanblob.database.windows.net,1433";
+$serverName = "tcp:appcen.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
     $row = 1;

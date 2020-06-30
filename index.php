d457293a7e336414e2f923de6fcd18575ae47999
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-30 18:30:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+  header('Content-Type: text/html; charset=ANSI');
+?>
+<?php
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "gunan", "pwd" => "app2020!", "Database" => "foodcloud", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:foodcloud.database.windows.net,1433";
+$connectionInfo = array("UID" => "gunan", "pwd" => "app2020!", "Database" => "food", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:appcen.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
     if (!$conn) {
         echo "conn: false";
@@ -38,13 +41,12 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
     <style>
         .map {
             width :100%;
-            height : 768px;
+            height : 50%;
         }
     </style>
     <title>맛집 검색</title>
 </head>
 <body>
-    <p>aaaa</p>
 <div id="map" class="map"></div>
 
 <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=fdda6bf48b2ef47a00384ad09b8c0684"></script>
@@ -57,6 +59,21 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+var positions = [
+    for(int i = 0; i < 530; i++){
+        latlng : new kakao.maps.LatLng()
+    }
+];
+for (var i = 0; i < positions.length; i ++) {
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image : markerImage // 마커 이미지 
+    });
+}
 </script>
 </body>
 </html>

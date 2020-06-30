c9d33770c8028d2124877f9a8529595bf229e916
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-30 19:00:59 +0900

    Update index.php

--- index.php
+++ index.php
@@ -57,23 +57,6 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         level: 9 // 지도의 확대 레벨
     };
 
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption); 
-
-var positions = [
-    for(int i = 0; i < 530; i++){
-        latlng : new kakao.maps.LatLng()
-    }
-];
-for (var i = 0; i < positions.length; i ++) {
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-        map: map, // 마커를 표시할 지도
-        position: positions[i].latlng, // 마커를 표시할 위치
-        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        image : markerImage // 마커 이미지 
-    });
-}
 </script>
 </body>
 </html>

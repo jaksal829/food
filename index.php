238725adb753fe6c9b7370373fdbc07b43ed575c
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-30 18:11:55 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <?php
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "gunan", "pwd" => "app2020!", "Database" => "food", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:appcen.database.windows.net,1433";
+$serverName = "tcp:foodcloud.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
     if (!$conn) {
         echo "conn: false";

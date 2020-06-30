2174a98ec2b46c3e526c56991591adab9bd11360
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-30 17:44:01 +0900

    Update index.php

--- index.php
+++ index.php
@@ -53,8 +53,8 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 <script>
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(35.161218, 129.047329), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        center: new kakao.maps.LatLng(35.179783, 129.075003), // 지도의 중심좌표
+        level: 9 // 지도의 확대 레벨
     };
 
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다

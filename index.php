33f22c521b8917846ed1f5bf748c7633b2d142af
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-30 17:42:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -39,11 +39,9 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 <head>
     <meta charset="utf-8">
     <style>
-        body{
-            height : 100%;
-        }
         .map {
             width :100%;
+            height : 768px;
         }
     </style>
     <title>맛집 검색</title>

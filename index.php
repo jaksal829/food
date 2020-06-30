17e92d7644fdd30e02764e8bece97064dbcb38b1
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-30 19:02:16 +0900

    Update index.php

--- index.php
+++ index.php
@@ -57,6 +57,7 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         level: 9 // 지도의 확대 레벨
     };
 
+var map = new kakao.maps.Map(mapContainer, mapOption); 
 </script>
 </body>
 </html>

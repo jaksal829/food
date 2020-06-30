0f13ff6204daed799a0a51efedde3a3bf3540b45
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-30 19:07:48 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -60,8 +60,9 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         level: 9 // 지도의 확대 레벨
     };
 
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+// 지도를 생성합니다    
 var map = new kakao.maps.Map(mapContainer, mapOption); 
+
 // 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder();
 

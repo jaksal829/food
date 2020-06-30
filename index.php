1a793ac6e8f402b8138f035f9dadf7563d5c3b99
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-30 16:11:23 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+header('Content-Type: text/html; charset=UTF-8');
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "gunan", "pwd" => "app2020!", "Database" => "app", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "gunan", "pwd" => "{your_password_here}", "Database" => "app", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:appcen.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
     if (!$conn) {
@@ -38,6 +39,6 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
     <title>맛집 검색</title>
 </head>
 <body>
-<p> 테트 </p>
+<p> 테스트 </p>
 </body>
 </html>
